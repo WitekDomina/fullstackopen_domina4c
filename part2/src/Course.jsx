7f734f9434418bc5e5e6aafe8acc39db997c7ca3
Witold Domina <witekdomina@gmail.com>
2025-02-21 14:32:09 +0100

    2.4

--- part2/src/Course.jsx
+++ part2/src/Course.jsx
@@ -3,12 +3,14 @@ import Header from './Header';
 import Content from './Content';
 
 const Course = ({ course }) => {
+  const totalExercises = course.parts.reduce((sum, part) => sum + part.exercises, 0);
   return (
     <div>
       <Header name={course.name} />
       <Content parts={course.parts} />
+      <p><strong>total of {totalExercises} exercises</strong></p>
     </div>
   );
-};
+}
 
 export default Course;
