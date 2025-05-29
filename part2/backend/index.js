9af42390934dbad0f8c7fc840b5b4cbb93f1e33d
Witold Domina <witekdomina@gmail.com>
2025-05-29 09:31:19 +0200

    3.10

--- part2/backend/index.js
+++ part2/backend/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 const persons = [
   { 
@@ -27,6 +33,10 @@ const persons = [
     number: "39-23-6423122"
   }
 ];
+
+app.get('/', (req, res) => {
+  res.send('Backend działa!');
+});
 
 app.get('/api/persons', (req, res) => {
   res.json(persons);
